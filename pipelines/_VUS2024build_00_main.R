6f40a344fdb939437ad548abc4635d455da746ec
AuroraSavino90 <103182819+AuroraSavino90@users.noreply.github.com>
2025-06-25 10:33:15 +0200

    00_main: change paths; update data to the latest version; use model_id instead of model_name to map across datasets; remove "other blood cancers" and "non-cancerous" from tissues.

--- pipelines/_VUS2024build_00_main.R
+++ pipelines/_VUS2024build_00_main.R
@@ -3,37 +3,51 @@ library(CELLector)
 library(tidyverse)
 
 ####setting paths
-pathdata <- "../data/_VUS2024build/"
+pathdata <- "data"
 pathscript <- "pipelines"
-resultPath<-'../results/_VUS2024build/20250617paperTest/'
+resultPath<-'results/20250221/'
 
 ###loading input data
-gene_annot <- read_csv(paste(pathdata, "/raw/gene_identifiers_20191101.csv", sep=""))
-### gene_identifiers_20191101 downloaded from https://cog.sanger.ac.uk/cmp/download/gene_identifiers_20191101.csv on 20241002
+gene_annot <- read_csv(paste(pathdata, "/raw/gene_identifiers_20241212.csv", sep=""))
+### gene_identifiers_20191101 downloaded from https://cog.sanger.ac.uk/cmp/download/gene_identifiers_20241212.csv on 20250221
 
-load(paste(pathdata,"/Robj/Sanger_Broad_higQ_scaled_depFC.RData", sep=""))
-load(paste(pathdata,'/Robj/Sanger_Broad_higQ_bdep.RData', sep=""))
-### Rbjects built from the essentiality matrices downloaded from https://cog.sanger.ac.uk/cmp/download/Project_score_combined_Sanger_v1_Broad_20Q2_20210311.zip on 20241002
+CMP_annot <- read_csv(paste(pathdata,"/raw/model_list_20241120.csv", sep="")) 
+### model_list_20240110.csv downloaded from https://cog.sanger.ac.uk/cmp/download/model_list_20241120.csv on 20250129
+
+scaled_depFC<-read.csv(paste(pathdata,'/raw/CRISPRGeneEffect.csv', sep=""), row.names = 1)
+colnames(scaled_depFC)<-gsub("\\..*","",colnames(scaled_depFC))
+###scaled essentiality matrices downloaded from https://depmap.org/portal/data_page/?tab=allData on 20250129 (24Q4)
+
+toremove<-which(is.na(CMP_annot$model_id[match(rownames(scaled_depFC),CMP_annot$BROAD_ID)]))
+scaled_depFC<-scaled_depFC[-toremove,]
+rownames(scaled_depFC)<-CMP_annot$model_id[match(rownames(scaled_depFC),CMP_annot$BROAD_ID)]
+
+scaled_depFC<-t(scaled_depFC)
+
+#remove genes with missing values
+scaled_depFC<-scaled_depFC[-which(rowSums(is.na(scaled_depFC))>0),]
+#create a binarized matrix (essential vs non-essential)
+bdep<-scaled_depFC
+bdep[scaled_depFC<=(-0.5)]<-1
+bdep[scaled_depFC>(-0.5)]<-0
 
 load(paste(pathdata,'/Robj/ADaM.RData', sep=""))
 load(paste(pathdata,'/Robj/FiPer_outputs.RData', sep=""))
 ### Rbjects precomputed as in Vinceti et al, BMC Genomics, 2021
 
-cl_variants <- read_csv(paste(pathdata,'/raw/mutations_all_20230202.csv', sep=""))
-### mutations_all_20230202 downloaded from  https://cog.sanger.ac.uk/cmp/download/mutations_all_20230202.zip on 20241002
+cl_variants <- read_csv(paste(pathdata,'/raw/mutations_all_20241212.csv', sep=""))
+### mutations_all_20230202 downloaded from  https://cog.sanger.ac.uk/cmp/download/mutations_all_20241212.zip on 20250221
 
 cl_variants <- cbind(cl_variants,gene_annot$hgnc_symbol[match(cl_variants$gene_id,gene_annot$gene_id)])
 colnames(cl_variants)[ncol(cl_variants)]<-'gene_symbol_2023'
 
-CMP_annot <- read_csv(paste(pathdata,"/raw/model_list_20240110.csv", sep="")) 
-### model_list_20240110.csv downloaded from https://cog.sanger.ac.uk/cmp/download/model_list_20240110.csv on 20241002
 
 ###print a few figures
 print(paste(nrow(CMP_annot),'annotated models in the Cell Models Passports'))
 print(paste('of which',length(which(is.element(CMP_annot$model_id,cl_variants$model_id))),'with mutation data'))
 
 #select only cell lines with data in both CRISPR screens and CMP annotation file
-CMP_annot<-CMP_annot[which(is.element(CMP_annot$model_name,colnames(scaled_depFC))),]
+CMP_annot<-CMP_annot[which(is.element(CMP_annot$model_id,colnames(scaled_depFC))),]
 print(paste('of which',nrow(CMP_annot),'with high quality CRISPR data'))
 
 #select tissues
@@ -41,16 +55,17 @@ tissues<-CMP_annot$cancer_type
 st<-summary(as.factor(tissues))
 
 tissues<-sort(setdiff(tissues,names(which(st<5))))
-tissues<-setdiff(tissues,c('Other Solid Carcinomas','Other Solid Cancers','Other Sarcomas'))
+tissues<-setdiff(tissues,c('Other Solid Carcinomas','Other Solid Cancers','Other Sarcomas', "Other Blood Cancers", "Non-Cancerous"))
 
 CMP_annot<-CMP_annot[which(is.element(CMP_annot$cancer_type,tissues)),]
 
 incl_cl_annot<-CMP_annot
 save(incl_cl_annot,file=paste(resultPath,'/_incl_cl_annot.RData',sep=''))
+write.table(incl_cl_annot,sep="\t",quote=FALSE,file=paste(resultPath,'_incl_cl_annot.tsv',sep=''))
 
 tissues<-sort(tissues)
 
-load(paste(pathdata,'Robj/basa_exp_FPKM.RData',sep=''))
+load(paste(pathdata, "/Robj/basal_exp.RData", sep=""))
 
 
 #run the main pipeline for each tissue separately
