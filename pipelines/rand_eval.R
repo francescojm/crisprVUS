1527d82d05691d9ded66b7aed39b45e44bb58a0a
AuroraSavino90 <103182819+AuroraSavino90@users.noreply.github.com>
2025-06-25 11:17:35 +0200

    Add empirical pvalues to the main results

--- pipelines/rand_eval.R
+++ pipelines/rand_eval.R
@@ -1,8 +1,19 @@
-setwd("/home/aurora.savino/VUS/VUS/results/20220208/rand_hits/")
-load("/home/aurora.savino/VUS/VUS/results/20220208/hits_nop.RData")
+library(tidyverse)
+#set path
+path_data<-"/data"
+path_results<-"/results/20250221"
+home<-"E:/VUS_2024build"
 
-rand_files<-list.files(pattern="rand")
-genes_rand<-matrix(nrow=length(hits), ncol=length(rand_files), dimnames=list(c(hits), c(gsub("_results_rand.RData", "", rand_files))))
+setwd(paste(home, "/", path_results, sep=""))
+
+load("hits_nop.RData")
+
+###############################
+### load randomization results
+################################
+
+rand_files<-list.files(pattern="rand", recursive = T)
+genes_rand<-matrix(nrow=length(hits_nop), ncol=length(rand_files), dimnames=list(c(hits_nop), c(gsub("_results_rand.RData", "", rand_files))))
 
 for(file in rand_files){
   print(file)
@@ -14,27 +25,14 @@ for(file in rand_files){
     genes<-c(genes,RESTOT_rand[[i]][[1]]$GENE[which(RESTOT_rand[[i]][[1]]$rank_ratio<1.6 & RESTOT_rand[[i]][[1]]$medFitEff< -.5)])
   }
   
-  genes_rand[names(table(genes)), gsub("_results_rand.RData", "", file)]<-table(genes)
+  genes_rand[intersect(names(table(genes)), hits_nop), gsub("VUS_rand/20250221/|_results_rand.RData", "", file)]<-table(genes)[intersect(names(table(genes)), hits_nop)]
 }
+
 genes_rand[is.na(genes_rand)]<-0
 save(genes_rand, file="genes_rand.RData")
 
 
-###################
-library(pheatmap)
-library(ggplot2)
-library(openxlsx)
-library(ReactomePA)
-library(clusterProfiler)
-library(tidyverse)
-
-path_data<-"/data"
-path_results<-"/results/20220208"
-home<-"/home/aurora.savino/VUS/VUS"
-
-setwd(paste(home, "/", path_results, sep=""))
-
-##load dei dati
+##load main results
 tissues<-gsub("_results.RData", "", list.files(pattern="results.RData"))
 
 results<-list()
@@ -46,11 +44,15 @@ for(ctiss in tissues){
 }
 names(results)<-tissues
 
-summary_hits<-matrix(0, nrow=length(hits), ncol=length(tissues))
-rownames(summary_hits)<-hits
+###############################
+### compute the empirical pvalue based on the randomizations
+################################
+
+summary_hits<-matrix(0, nrow=length(hits_nop), ncol=length(tissues))
+rownames(summary_hits)<-hits_nop
 colnames(summary_hits)<-tissues
 
-for(dg in hits){
+for(dg in hits_nop){
   for(ctiss in tissues){
     if(dg %in% results[[ctiss]]$GENE[results[[ctiss]]$rank_ratio<1.6 & results[[ctiss]]$medFitEff< -.5]){
       summary_hits[dg,ctiss]<-1
@@ -60,12 +62,6 @@ for(dg in hits){
 }
 
 summary_pval<-genes_rand/1000
-summary_fdr<-apply(summary_pval,2,function(x){p.adjust(x,method="fdr")})
-hist(-log10(summary_fdr[summary_hits==1]), breaks=100)
-
-hits_filt<-rownames(summary_fdr)[which(summary_fdr<0.1 & summary_hits==1, arr.ind=T)[,1]]
-
-sort(table(hits_filt))
 
 for(ctiss in tissues){
    load(paste(ctiss, "_results.RData", sep=""))
@@ -74,5 +70,25 @@ for(ctiss in tissues){
   RESTOT[match(incommon,RESTOT$GENE), "pval_rand"]<-summary_pval[incommon,ctiss]
   save(RESTOT, file=paste(ctiss, "_results_ext.RData", sep=""))
 }
+
+##load DAM data with p
+tissues<-gsub("_results_ext.RData", "", list.files(pattern="results_ext.RData"))
+
+results<-list()
+ind_iter<-0
+for(ctiss in tissues){
+  ind_iter<-ind_iter+1
+  load(paste(ctiss, "_results_ext.RData", sep=""))
+  results[[ind_iter]] <- RESTOT
+}
+names(results)<-tissues
+
+hits<-c()
+for(ctiss in tissues){
+  hits<-c(hits, unique(results[[ctiss]]$GENE[results[[ctiss]]$rank_ratio<1.6 & results[[ctiss]]$medFitEff< -.5 & results[[ctiss]]$pval_rand<0.2 ]))
+}
+
+hits<-unique(hits)
+save(hits, file="hits.RData")
 
 
